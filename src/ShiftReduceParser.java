ccff40dc222c7220f08e0d756f30f5d27857f84f
Demilade <demiladeoladugba@gmail.com>
2021-04-27 13:13:24 +0100

    added iteration for reducer

--- src/ShiftReduceParser.java
+++ src/ShiftReduceParser.java
@@ -1,6 +1,5 @@
 import models.enums.ShiftActions;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -16,28 +15,28 @@ public class ShiftReduceParser {
         for (String token : tokens) {
             stack = stack + token;
             System.out.printf("%25s|%25s|%25s\n", stack, "Input", "Shift");
-            reduce();
+            Pattern grammarRegex = Pattern.compile("\\d+|-T|\\+T|D|T\\.D|T\\*10\\^T");
+            while (grammarRegex.matcher(stack).find()) {
+                reduce();
+            }
         }
         return ShiftActions.ERROR;
     }
 
     private void reduce() {
 
-        Pattern digitRegex = Pattern.compile("(\\d+)");
+        Pattern digitRegex = Pattern.compile("\\d+");
         Pattern termRegex = Pattern.compile("-T|\\+T|D|T\\.D");
         Pattern sdRegex = Pattern.compile("T\\*10\\^T");
         if (digitRegex.matcher(stack).find()) {
             stack = stack.replaceFirst(digitRegex.pattern(), "D");
-//            if ()
-            System.out.printf("%25s|%25s|%25s\n", stack, "Input", "Reduce");
         }
         if (termRegex.matcher(stack).find()) {
             stack = stack.replaceFirst(termRegex.pattern(), "T");
-            System.out.printf("%25s|%25s|%25s\n", stack, "Input", "Reduce");
         }
         if (sdRegex.matcher(stack).find()) {
             stack = stack.replaceFirst(sdRegex.pattern(), "S");
-            System.out.printf("%25s|%25s|%25s\n", stack, "Input", "Reduce");
         }
+        System.out.printf("%25s|%25s|%25s\n", stack, "Input", "Reduce");
     }
 }
