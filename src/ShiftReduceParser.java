ec3359926cd88e65e353853d8d9a9a71571f3979
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-04-27 14:39:39 +0100

    Modified the grammar and the parsing table

--- src/ShiftReduceParser.java
+++ src/ShiftReduceParser.java
@@ -7,25 +7,28 @@ public class ShiftReduceParser {
     private String stack;
 
     public ShiftActions parse(List<String> tokens) {
-        System.out.printf("%25s|%25s|%25s\n", "Stack", "Input", "Action");
-//        System.out.printf("%25s|%25s|%25s\n", stack, input, "Shift");
+        stack = "$";
+        tokens.add("$");
+
+        System.out.printf("%30s|%30s|%30s\n", "Stack", "Input", "Action");
+        System.out.printf("%30s|%30s|%30s\n", stack, tokens, "");
         for (int i = 0; i < tokens.size(); i++) {
             stack = stack + tokens.get(i);
-            System.out.printf("%25s|%25s|%25s\n", stack, String.join("", tokens.subList(i + 1, tokens.size())) + "$", "Shift");
-            Pattern grammarRegex = Pattern.compile("\\d+|-T|\\+T|D|T\\.D|T\\*10\\^T");
+            System.out.printf("%30s|%30s|%30s\n", stack, tokens.subList(i+1, tokens.size()), "Shift");
+            Pattern grammarRegex = Pattern.compile("\\d+|-T|\\+T|N|T\\.N|T\\*10\\^T");
             while (grammarRegex.matcher(stack).find()) {
-                reduce(String.join("", tokens.subList(i + 1, tokens.size())) + "$");
+                reduce(tokens.subList(i+1, tokens.size()).toString());
             }
         }
         return ShiftActions.ERROR;
     }
 
     private void reduce(String input) {
-        Pattern digitRegex = Pattern.compile("\\d+");
-        Pattern termRegex = Pattern.compile("-T|\\+T|D|T\\.D");
+        Pattern numberRegex = Pattern.compile("\\d+\\.\\d+|\\d+");
+        Pattern termRegex = Pattern.compile("-T|\\+T|N");
         Pattern sdRegex = Pattern.compile("T\\*10\\^T");
-        if (digitRegex.matcher(stack).find()) {
-            stack = stack.replaceFirst(digitRegex.pattern(), "D");
+        if (numberRegex.matcher(stack).find()) {
+            stack = stack.replaceFirst(numberRegex.pattern(), "N");
         }
         if (termRegex.matcher(stack).find()) {
             stack = stack.replaceFirst(termRegex.pattern(), "T");
@@ -33,6 +36,6 @@ public class ShiftReduceParser {
         if (sdRegex.matcher(stack).find()) {
             stack = stack.replaceFirst(sdRegex.pattern(), "S");
         }
-        System.out.printf("%25s|%25s|%25s\n", stack, input, "Reduce");
+        System.out.printf("%30s|%30s|%30s\n", stack, input, "Reduce");
     }
 }
